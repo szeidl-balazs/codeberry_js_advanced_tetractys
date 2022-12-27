0a80b0c0dfb75b6316461b2668c7d5a9e0f8cfc6
bszeidl <szeidl.balazs@hotmail.com>
2022-12-27 23:09:19 +0100

    The 7 hexagons are ready.

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
-    var context = canvas.getContext('2d');    
+    var context = canvas.getContext('2d');
     
+    let width = 113;
+    let height = 98;
     
+    function drawHexagon(positionX, positionY) {
+
+        context.beginPath();
+        context.moveTo(positionX, positionY);
+        context.lineTo(positionX + width * .25, positionY - height * .5);
+        context.lineTo(positionX + width * .75, positionY - height * .5);
+        context.lineTo(positionX + width, positionY);
+        context.lineTo(positionX + width * .75, positionY + height * .5);
+        context.lineTo(positionX + width * .25, positionY + height * .5);
+        context.lineTo(positionX, positionY);
+
+        context.strokeStyle = 'orange';
+        context.stroke();        
+
+    }
     
-    function lineToCenter (positionX, positionY, color) {     
-            
-            context.beginPath();
-            context.moveTo(positionX,positionY);
-            context.lineTo(canvas.width * .5, canvas.height * .5);
-            context.strokeStyle = color;
-            context.stroke();
-
-            console.log(positionX,positionY);                
-    }    
-
-    const random = (max, min) => {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-     }
-
-     /*A lineToCenter függvény meghívásakor argumentumként hívom meg a random függvényt annak két 
-     argumentumával és a random függvény által létrehozott érték kerül be a lineToCenter 
-     függvénybe paraméterként. */
-
-    for (i = 0; i <= 1000; i++) {
-        lineToCenter(random(canvas.width,0),random(canvas.height,0),'teal');
-     } 
-    
+    drawHexagon(76, 120);
+    drawHexagon(76, 230);
+    drawHexagon(168.5, 65);   
+    drawHexagon(168.5, 175);
+    drawHexagon(168.5, 285);
+    drawHexagon(261, 120);
+    drawHexagon(261, 230);
+    /*Csak azt a síkidomot színezi ki, amelyik meghívása után adjuk meg a színezés kódját. */
+    context.fillStyle = 'orange';
+    context.fill();   
     
 }
 
