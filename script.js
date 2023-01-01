f62d7940f8497f54919d62e1547ce51c17f4bab3
bszeidl <szeidl.balazs@hotmail.com>
2023-01-02 00:29:34 +0100

    The six triangles are ready.

--- script.js
+++ script.js
@@ -5,23 +5,45 @@ function pageLoaded () {
     let base = 100;
     let height = 86.6;
     
-    function drawTriangle (positionX, positionY) {
+    
+    function drawTriangle (x,y) {       
 
-        for (i = 0; i < 3; i++) {
+            
+        for (var row = 0; row < 3; row++) {
 
-            context.moveTo(positionX - i * base * .5, positionY + i * height);
-            context.lineTo(positionX + base * .5, positionY + height);
-            context.lineTo(positionX - base * .5, positionY + height);
-            context.lineTo(positionX, positionY);
+            for (var column = 0; column <= row; column++) {
 
+                context.beginPath();
+                
+                context.moveTo(x - row * base * .5 + column * base, y + row * height);                
+                context.lineTo(x - base * (row + 1) * .5 + column * base, y + (row + 1) * height);
+                context.lineTo(x - base * (row - 1) * .5 + column * base, y + (row + 1) * height);
+                context.lineTo(x - row * base * .5 + column * base, y + row * height);
+                
+                if (row === 0) {
 
+                    context.strokeStyle = 'rgb(227,98,102)';
+                    context.fillStyle = 'rgb(227,98,102)';
+                    
+                } else if ( row === 1) {
+    
+                    context.strokeStyle = 'rgb(38,172,73)';
+                    context.fillStyle = 'rgb(38,172,73)';
+    
+                } else {
+    
+                    context.strokeStyle = 'rgb(34,128,128)';
+                    context.fillStyle = 'rgb(34,128,128)';
+    
+                }
+
+                context.stroke();
+                context.fill();
+                
+
+            }
+ 
         }
-
-
-
-
-        context.stroke();
-
 
     }
 
